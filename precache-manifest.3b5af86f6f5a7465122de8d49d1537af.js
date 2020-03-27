047cdb79be240e89c0fcbdc016018fe112a5155c
John Abreu <abreuja@bc.edu>
2020-03-27 18:48:01 -0400

    Updates

---
+++ precache-manifest.3b5af86f6f5a7465122de8d49d1537af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672c2a94b9dfa4a789948c8646d72332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d139396a07f8a4de184",
+    "url": "/static/css/main.1b03eb9d.chunk.css"
+  },
+  {
+    "revision": "fe04d8847d5f244363db",
+    "url": "/static/js/2.11895b5a.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/static/js/2.11895b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d139396a07f8a4de184",
+    "url": "/static/js/main.5c2e2d02.chunk.js"
+  },
+  {
+    "revision": "6d3723ef8c6ed189760b",
+    "url": "/static/js/runtime-main.4bece70b.js"
+  }
+]);
